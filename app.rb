19bb31682e70fe5950c505c3d5ff09eb30b70d7d
Shawn Ren <shawnren527@gmail.com>
2019-04-23 02:54:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,11 +5,9 @@ class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
   end
-
-  post '/food' do
-    
-  end
-
   # Add your post route and action below
+  post '/food' do
+
+  end
 
 end
